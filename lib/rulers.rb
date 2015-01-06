2430f7f62ff319537fcaa33f99da2fed87b4ff62
Josh Greenwood <joshua.t.greenwood@gmail.com>
2015-01-05 20:20:55 -0500

    Adds array#sum, debug, and hello world

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,5 +1,14 @@
-require "rulers/version"
+require 'rulers/version'
+require 'rulers/array'
 
 module Rulers
-  # Your code goes here...
+  class Application
+    def call(env)
+      `echo debug > debug.txt`
+      [200,
+       {'Content-Type' => 'text/html'},
+       ["Hello from Ruby On Rulers!"]
+      ]
+    end
+  end
 end
